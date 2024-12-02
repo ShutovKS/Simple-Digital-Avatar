ef7898e0811841b49ce71208683505bc0d0328a3
ShutovKS <schutow.kir@yandex.ru>
2024-12-02 06:04:01 +0300

    Add ServiceLocator

---
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/ServiceLocator.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+public class ServiceLocator
+{
+    public static ServiceLocator Instance => _instance ??= new ServiceLocator();
+    private static ServiceLocator _instance;
+
+    private readonly Dictionary<Type, object> _services = new();
+
+    public void Register<T>(object service) => _services[typeof(T)] = service;
+
+    public T Get<T>() => (T)_services[typeof(T)];
+
+    public void Clear() => _services.Clear();
+
+    public void Unregister<T>() => _services.Remove(typeof(T));
+
+    public bool Contains<T>() => _services.ContainsKey(typeof(T));
+}
