7bcd05cd04d653482e5805111abfe000d9e3c24d
ShutovKS <schutow.kir@yandex.ru>
2024-12-02 06:05:43 +0300

    Add DigitalAvatar

---
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/DigitalAvatar.cs
@@ -0,0 +1,32 @@
+﻿using System.Threading.Tasks;
+using UnityEngine;
+
+public class DigitalAvatar : MonoBehaviour
+{
+    [SerializeField] private TextDisplay textDisplay;
+
+    private IAudioRecorder _audioRecorder;
+    private ISpeechRecognizer _speechRecognizer;
+
+    private void Start()
+    {
+        var serviceLocator = ServiceLocator.Instance;
+        _audioRecorder = serviceLocator.Get<IAudioRecorder>();
+        _speechRecognizer = serviceLocator.Get<ISpeechRecognizer>();
+
+        _audioRecorder.OnAudioDataReceived += OnAudioDataReceived;
+    }
+
+    private async void OnAudioDataReceived(float[] audioData)
+    {
+        var text = await RecognizeSpeechAsync(audioData);
+        textDisplay.SetText(text);
+    }
+
+    private async Task<string> RecognizeSpeechAsync(float[] audioData)
+    {
+        if (audioData == null) return string.Empty;
+
+        return await _speechRecognizer.GetTextAsync(audioData);
+    }
+}
