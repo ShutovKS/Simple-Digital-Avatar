7c4ad5a9a6e2eb6ab14832d62d26f945cc40e1aa
ShutovKS <schutow.kir@yandex.ru>
2024-12-02 06:03:38 +0300

    Add AudioRecorder

---
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/AudioRecorder.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using UnityEngine;
+using Object = UnityEngine.Object;
+
+public interface IAudioRecorder
+{
+    event Action<float[]> OnAudioDataReceived;
+
+    void StartRecording();
+    void StopRecording();
+    float[] ReadAudioData();
+
+    void SetMicrophoneIndex(int index);
+}
+
+public class AudioRecorder : IAudioRecorder
+{
+    public event Action<float[]> OnAudioDataReceived;
+
+    private int _microphoneIndex;
+    private AudioClip _microphoneClip;
+    private int _position1, _position2;
+
+    public void SetMicrophoneIndex(int index)
+    {
+        _microphoneIndex = index;
+    }
+
+    public void StartRecording()
+    {
+        if (_microphoneClip != null)
+        {
+            Object.Destroy(_microphoneClip);
+        }
+
+        _microphoneClip = Microphone.Start(Microphone.devices[_microphoneIndex], true, 3500, 16000);
+        _position1 = 0;
+    }
+
+    public void StopRecording()
+    {
+        if (_microphoneClip == null) return;
+
+        OnAudioDataReceived?.Invoke(GetAudioData());
+
+        Microphone.End(Microphone.devices[_microphoneIndex]);
+
+        _microphoneClip = null;
+
+        _position1 = 0;
+        _position2 = 0;
+    }
+
+    public float[] ReadAudioData()
+    {
+        if (_microphoneClip == null || !Microphone.IsRecording(Microphone.devices[_microphoneIndex])) return null;
+
+        _position2 = Microphone.GetPosition(Microphone.devices[_microphoneIndex]);
+
+        if (_position2 <= _position1) return null;
+
+        var audioData = new float[_position2 - _position1];
+        _microphoneClip.GetData(audioData, _position1);
+
+        _position1 = _position2;
+
+        return audioData;
+    }
+
+    private float[] GetAudioData()
+    {
+        _position2 = Microphone.GetPosition(Microphone.devices[_microphoneIndex]);
+
+        if (_position2 <= 0) return null;
+
+        var audioData = new float[_position2];
+        _microphoneClip.GetData(audioData, 0);
+        return audioData;
+    }
+}
