174f0dad84d272167741211c52fbcbb3381a8efc
ShutovKS <schutow.kir@yandex.ru>
2024-12-02 14:13:45 +0300

    Add SpeechRecognizer

---
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/ConversationGeneration.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Threading.Tasks;
+using LLMUnity;
+
+public interface IConversationGeneration
+{
+    bool IsGenerating { get; }
+
+    event Action<string> OnMessageReceived;
+    event Action OnMessageReceivedCompleted;
+
+    Task<string> StartGeneration(string message);
+    void StopGeneration();
+}
+
+public class ConversationGeneration : IConversationGeneration
+{
+    public event Action<string> OnMessageReceived;
+    public event Action OnMessageReceivedCompleted;
+
+    public bool IsGenerating { get; private set; }
+
+    private readonly LLMCharacter _llmCharacter = ServiceLocator.Instance.Get<LLMCharacter>();
+
+    public async Task<string> StartGeneration(string message)
+    {
+        if (IsGenerating) return null;
+
+        IsGenerating = true;
+
+        return await _llmCharacter.Chat(message, MessageReceived, MessageReceivedComplete);
+    }
+
+    public void StopGeneration()
+    {
+        _llmCharacter.CancelRequests();
+
+        IsGenerating = false;
+    }
+
+    private void MessageReceived(string message)
+    {
+        OnMessageReceived?.Invoke(message);
+    }
+
+    private void MessageReceivedComplete()
+    {
+        IsGenerating = false;
+        OnMessageReceivedCompleted?.Invoke();
+    }
+}
--- src/Simple Digital Avatar/Assets/!Project Files/Scripts/DependencyInstaller.cs
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/DependencyInstaller.cs
@@ -1,20 +1,26 @@
-﻿using UnityEngine;
+﻿using LLMUnity;
+using UnityEngine;
 using Whisper;
 
 public class DependencyInstaller : MonoBehaviour
 {
     [SerializeField] private WhisperManager whisperManager;
+    [SerializeField] private LLMCharacter llmCharacter;
     
     private void Awake()
     {
         var serviceLocator = ServiceLocator.Instance;
 
         serviceLocator.Register<WhisperManager>(whisperManager);
+        serviceLocator.Register<LLMCharacter>(llmCharacter);
         
         var audioRecorder = new AudioRecorder();
         serviceLocator.Register<IAudioRecorder>(audioRecorder);
         
         var speechRecognizer = new SpeechRecognizer();
         serviceLocator.Register<ISpeechRecognizer>(speechRecognizer);
+        
+        var conversationGeneration = new ConversationGeneration();
+        serviceLocator.Register<IConversationGeneration>(conversationGeneration);
     }
 }
