4ebc1b5f9cc1e685ddc46ad1b308d7da0d4565b6
ShutovKS <schutow.kir@yandex.ru>
2024-12-02 06:04:47 +0300

    Add Microphone button and handler

---
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/MicrophoneButton.cs
@@ -0,0 +1,29 @@
+using System;
+using UnityEngine;
+using UnityEngine.UI;
+
+[RequireComponent(typeof(Button))]
+public class MicrophoneButton : MonoBehaviour
+{
+    public event Action OnMicrophoneToggle;
+
+    private Button _button;
+
+    private void Awake()
+    {
+        _button = GetComponent<Button>();
+    }
+
+    private void OnEnable()
+    {
+        _button.onClick.AddListener(ToggleMicrophone);
+    }
+
+    private void OnDisable()
+    {
+        _button.onClick.RemoveListener(ToggleMicrophone);
+    }
+
+    private void ToggleMicrophone() =>
+        OnMicrophoneToggle?.Invoke();
+}
---
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/MicrophoneButtonHandler.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(MicrophoneButton))]
+public class MicrophoneButtonHandler : MonoBehaviour
+{
+    private void Awake()
+    {
+        var microphoneButton = GetComponent<MicrophoneButton>();
+        microphoneButton.OnMicrophoneToggle += OnMicrophoneToggle;
+    }
+
+    private void OnMicrophoneToggle()
+    {
+        var serviceLocator = ServiceLocator.Instance;
+        var audioRecorder = serviceLocator.Get<IAudioRecorder>();
+
+        if (Microphone.IsRecording(Microphone.devices[0]))
+        {
+            audioRecorder.StopRecording();
+        }
+        else
+        {
+            audioRecorder.StartRecording();
+        }
+    }
+}
