79a36db35f6c7ff2d62ad9df8b7a14ac8b4427d6
ShutovKS <schutow.kir@yandex.ru>
2024-12-02 06:05:32 +0300

    Add DependencyInstaller

---
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/DependencyInstaller.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using Whisper;
+
+public class DependencyInstaller : MonoBehaviour
+{
+    [SerializeField] private WhisperManager whisperManager;
+    
+    private void Awake()
+    {
+        var serviceLocator = ServiceLocator.Instance;
+
+        serviceLocator.Register<WhisperManager>(whisperManager);
+        
+        var audioRecorder = new AudioRecorder();
+        serviceLocator.Register<IAudioRecorder>(audioRecorder);
+        
+        var speechRecognizer = new SpeechRecognizer();
+        serviceLocator.Register<ISpeechRecognizer>(speechRecognizer);
+    }
+}
