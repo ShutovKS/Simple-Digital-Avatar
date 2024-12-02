04335d067a5aca92ea5022f154d5fdf3ed03ea13
ShutovKS <schutow.kir@yandex.ru>
2024-12-02 06:04:11 +0300

    Add SpeechRecognizer

---
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/SpeechRecognizer.cs
@@ -0,0 +1,18 @@
+﻿using System.Threading.Tasks;
+using Whisper;
+
+public interface ISpeechRecognizer
+{
+    Task<string> GetTextAsync(float[] samples);
+}
+
+public class SpeechRecognizer : ISpeechRecognizer
+{
+    private readonly WhisperManager _whisperManager = ServiceLocator.Instance.Get<WhisperManager>();
+
+    public async Task<string> GetTextAsync(float[] samples)
+    {
+        var whisperResult = await _whisperManager.GetTextAsync(samples, 16000, 1);
+        return whisperResult.Result;
+    }
+}
