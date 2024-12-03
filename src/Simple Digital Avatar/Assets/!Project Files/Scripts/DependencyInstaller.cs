d5824c86e5ec68294b57500f8c4d7d39e4df4d07
ShutovKS <schutow.kir@yandex.ru>
2024-12-04 02:46:06 +0300

    Add new scenes and scene loader

--- src/Simple Digital Avatar/Assets/!Project Files/Scripts/DependencyInstaller.cs
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/DependencyInstaller.cs
@@ -1,26 +1,43 @@
-﻿using LLMUnity;
+﻿using System;
+using System.Threading.Tasks;
+using LLMUnity;
+using Unity.Sentis;
 using UnityEngine;
 using Whisper;
 
 public class DependencyInstaller : MonoBehaviour
 {
+    public event Action OnDependenciesInstalled;
+
     [SerializeField] private WhisperManager whisperManager;
     [SerializeField] private LLMCharacter llmCharacter;
-    
-    private void Awake()
+    [SerializeField] private ModelAsset model;
+    [SerializeField] private string language;
+    [SerializeField] private int sampleRate;
+
+    private async void Start()
     {
+        DontDestroyOnLoad(gameObject);
+
         var serviceLocator = ServiceLocator.Instance;
 
         serviceLocator.Register<WhisperManager>(whisperManager);
         serviceLocator.Register<LLMCharacter>(llmCharacter);
-        
+
         var audioRecorder = new AudioRecorder();
         serviceLocator.Register<IAudioRecorder>(audioRecorder);
-        
+
         var speechRecognizer = new SpeechRecognizer();
         serviceLocator.Register<ISpeechRecognizer>(speechRecognizer);
-        
+
         var conversationGeneration = new ConversationGeneration();
         serviceLocator.Register<IConversationGeneration>(conversationGeneration);
+
+        var speechSynthesis = new SpeechSynthesisWithPiper(model, language, sampleRate);
+        serviceLocator.Register<ISpeechSynthesis>(speechSynthesis);
+
+        await Task.Delay(1000);
+
+        OnDependenciesInstalled?.Invoke();
     }
 }
---
+++ src/Simple Digital Avatar/Assets/!Project Files/Scripts/SceneLoader.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneLoader : MonoBehaviour
+{
+    [SerializeField] private string sceneName;
+    [SerializeField] private DependencyInstaller dependencyInstaller;
+
+    private void Awake()
+    {
+        dependencyInstaller.OnDependenciesInstalled += LoadScene;
+    }
+
+    private void LoadScene()
+    {
+        SceneManager.LoadScene(sceneName);
+    }
+}
